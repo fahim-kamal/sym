4f2ab9b3f56a97a6c15507967dab3d638c16e56c
Fahim Kamal <fkamal@usc.edu>
2024-06-23 15:53:54 -0700

    fix: refactors error types
    Refactors error interface so that name is accessible on the class. This
    ensures that we are able to set the name within the class of a specific
    error but still have access to name without initializing an error of that
    specific type.

--- src/entities/errors.ts
+++ src/entities/errors.ts
@@ -1,14 +1,22 @@
-export class AuthenticationError extends Error {
-  constructor(message: string = "") {
-    const defaultMessage = `User is not authenticated.`;
-    super(message != "" ? message : defaultMessage);
-    this.name = "AuthenticationError";
-  }
+interface DomainError extends Error {
+  name: string;
 }
 
-export class UserPageNumberExceeded extends Error {
+export class AuthenticationError implements DomainError {
+  message: "User is not authenticated";
+  name: "AuthenticationError";
+}
+
+export class UserPageNumberExceeded implements DomainError {
   constructor(maxNumPages: number) {
-    super(`User cannot exceed ${maxNumPages} pages for current plan.`);
-    this.name = "UserPageNumberExceeded";
+    this.message = `User cannot exceed ${maxNumPages} pages for current plan.`;
   }
+
+  name = "UserPageNumberExceeded";
+  message: string;
+}
+
+export class InvalidPageName implements DomainError {
+  name = "InvalidPageName";
+  message = "Page name cannot be an empty string or exceed 125 characters.";
 }
