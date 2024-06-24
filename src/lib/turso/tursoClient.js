245714c56c3d07e687aaf1488909d3b9a5fea8fc
Fahim Kamal <fkamal@usc.edu>
2024-06-24 11:40:22 -0700

    fix: changes env variable selection for tursoClient when in test mode

--- src/lib/turso/tursoClient.js
+++ src/lib/turso/tursoClient.js
@@ -1,6 +1,12 @@
 import { createClient } from "@libsql/client";
 
 export const tursoClient = createClient({
-  url: process.env.NEXT_PUBLIC_TURSO_DATABASE_URL,
-  authToken: process.env.NEXT_PUBLIC_TURSO_AUTH_TOKEN,
+  url:
+    process.env.NODE_ENV === "test"
+      ? import.meta.env.VITE_NEXT_PUBLIC_TURSO_DATABASE_URL
+      : process.env.NEXT_PUBLIC_TURSO_DATABASE_URL,
+  authToken:
+    process.env.NODE_ENV === "test"
+      ? import.meta.env.VITE_NEXT_PUBLIC_TURSO_AUTH_TOKEN
+      : process.env.NEXT_PUBLIC_TURSO_AUTH_TOKEN,
 });
