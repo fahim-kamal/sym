4eef13c5f977eecb5d42e125f97931eb3f9c4d87
Fahim Kamal <fkamal@usc.edu>
2024-06-24 11:44:31 -0700

    fix: changes route to include a better message if the resource could not be found

--- src/app/api/v1/goal_page/[goal_id]/route.ts
+++ src/app/api/v1/goal_page/[goal_id]/route.ts
@@ -1,9 +1,10 @@
-import type { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
+import { auth } from "@/lib/auth/auth";
 
 import { getGoalPageByIdCase } from "@/use-cases/getGoalPageByIdCase";
+import { deletePageCase } from "@/use-cases/deletePageCase";
+
 import { TursoGoalPageRepo } from "@/db-access/goalPageRepository";
-import { auth } from "@/lib/auth/auth";
 import { NextErrorController } from "@/controllers/errorController";
 
 export const GET = auth(async function GET(
@@ -21,5 +22,29 @@ export const GET = auth(async function GET(
     return NextResponse.json({ page }, { status: 200 });
   } catch (error) {
     return NextErrorController.handleError(error);
+  }
+});
+
+export const DELETE = auth(async function DELETE(
+  req,
+  ctx: { params: { goal_id: string } }
+) {
+  const { goal_id } = ctx.params;
+  const user_id = req.auth?.user.id;
+
+  try {
+    const deleted_id = await deletePageCase(
+      { goalPageRepo: new TursoGoalPageRepo() },
+      { isAuthenticated: Boolean(req.auth) !== null, goal_id, user_id }
+    );
+
+    const message =
+      deleted_id !== undefined
+        ? "Succesfully deleted " + goal_id
+        : "Could not find " + goal_id;
+
+    return NextResponse.json({ message }, { status: 200 });
+  } catch (err) {
+    return NextErrorController.handleError(err);
   }
 });
