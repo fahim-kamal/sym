04f877cb8db70c1e6433321015095fbad43c4b74
Fahim Kamal <fkamal@usc.edu>
2024-06-24 19:47:00 -0700

    feat: adds border to header

--- src/components/symHeader.js
+++ src/components/symHeader.js
@@ -2,7 +2,7 @@ import SymLogo from "./symLogo";
 
 export default function SymHeader() {
   return (
-    <div className="h-16 flex flex-row items-center mx-20">
+    <div className="h-16 flex flex-row items-center px-20 border-b border-gray-400 bg-white">
       <SymLogo />
     </div>
   );
