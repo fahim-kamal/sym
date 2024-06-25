f78fcbf2425a42138088404fab88a3407f3eecd0
Fahim Kamal <fkamal@usc.edu>
2024-06-24 19:47:51 -0700

    feat: creates goalSection component for sidebar

---
+++ src/app/dashboard/goalSection.js
@@ -0,0 +1,42 @@
+import AddIcon from "@/components/addIcon";
+
+export const testGoals = [
+  { name: "Working Out Everyday" },
+  { name: "Drinking More Water" },
+  { name: "Saving Money" },
+];
+
+function GoalItem({ name }) {
+  return (
+    <div className="truncate px-4 py-1 hover:bg-sym_bg_gray hover:cursor-pointer">
+      {name}
+    </div>
+  );
+}
+
+function GoalsList({ goalsList }) {
+  return (
+    <div className="flex flex-col">
+      {goalsList.map(({ name }) => (
+        <GoalItem name={name} />
+      ))}
+    </div>
+  );
+}
+
+export default function GoalsSection({ goals }) {
+  return (
+    <div className="py-4">
+      <div className="flex flex-row justify-between px-4">
+        <h3>Your Goals</h3>
+        <AddIcon />
+      </div>
+
+      {goals.length ? (
+        <GoalsList goalsList={goals} />
+      ) : (
+        <div className="px-4">Start tracking a goal</div>
+      )}
+    </div>
+  );
+}
