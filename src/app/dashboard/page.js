cc7d32713298a2b062d03f351a3be65d7dbd7a0a
Fahim Kamal <fkamal@usc.edu>
2024-06-24 19:49:11 -0700

    feat: adds sidebar to dashboard page

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,10 +1,14 @@
 import SymHeader from "@/components/symHeader";
+import GoalList from "./goalList";
+import Sidebar from "./sidebar";
+import ReactQueryProvider from "@/context/queryContext";
 
 export default function Dashboard() {
   return (
-    <div>
+    <div className="bg-sym_bg_gray h-dvh flex flex-col">
       <SymHeader />
-      <div>hey</div>
+      <Sidebar />
+      <ReactQueryProvider>{/* <GoalList /> */}</ReactQueryProvider>
     </div>
   );
 }
