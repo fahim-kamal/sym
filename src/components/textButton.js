2116246c03deac83347699f6eb7d843fa26f2680
Fahim Kamal <fkamal@usc.edu>
2024-07-22 22:54:53 -0700

    feat: enables editing width on text button

--- src/components/textButton.js
+++ src/components/textButton.js
@@ -1,4 +1,9 @@
-export default function TextButton({ text, variant = "outline", onClick }) {
+export default function TextButton({
+  text,
+  variant = "outline",
+  width = "w-max",
+  onClick,
+}) {
   const variantStyles = {
     outline: "border border-black text-sm",
     solid: "bg-black text-white text-sm",
@@ -8,8 +13,9 @@ export default function TextButton({ text, variant = "outline", onClick }) {
     <div
       onClick={onClick}
       className={
-        "px-2 py-1 grid w-max rounded grid-flow-col auto-cols-max gap-x-4 items-center hover:cursor-pointer " +
-        variantStyles[variant]
+        "px-2 py-1 rounded flex justify-center gap-x-4 items-center hover:cursor-pointer " +
+        variantStyles[variant] +
+        ` ${width}`
       }
     >
       <div className="select-none">{text}</div>
