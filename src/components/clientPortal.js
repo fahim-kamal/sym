add945ed7c7853b55dfbd98f0383fc95ff474c1a
Fahim Kamal <fkamal@usc.edu>
2024-07-22 22:53:46 -0700

    feat: adds client portal component

---
+++ src/components/clientPortal.js
@@ -0,0 +1,16 @@
+"use client";
+
+import { useState, useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+export default function ClientPortal({ children, selector }) {
+  const ref = useRef();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    ref.current = document.querySelector(selector);
+    setMounted(true);
+  }, [selector]);
+
+  return mounted ? createPortal(children, ref.current) : null;
+}
