9cd2c58fd18a151c18d51898b2dd470619b944ea
Fahim Kamal <fkamal@usc.edu>
2024-07-22 22:52:28 -0700

    feat: implements modal form for adding a new goal page

--- src/app/dashboard/goalSection.js
+++ src/app/dashboard/goalSection.js
@@ -1,4 +1,15 @@
+"use client";
+
+import ClientPortal from "@/components/clientPortal";
 import AddIcon from "@/components/icons/addIcon";
+import CloseIcon from "@mui/icons-material/Close";
+
+import { createPortal } from "react-dom";
+import { useToggle } from "@/hooks/useToggle";
+
+import TextButton from "@/components/textButton";
+
+import { useRouter } from "next/navigation";
 
 export const testGoals = [
   { name: "Working Out Everyday" },
@@ -18,8 +29,84 @@ function GoalsList({ goalsList }) {
   return (
     <div className="flex flex-col">
       {goalsList.map(({ name }) => (
-        <GoalItem name={name} />
+        <GoalItem name={name} key={name} />
       ))}
+    </div>
+  );
+}
+
+function LightboxMenu({ children, showLightbox, closeLightbox }) {
+  return showLightbox ? (
+    <>
+      <div
+        className="absolute w-screen h-screen bg-black/10"
+        onClick={closeLightbox}
+      />
+      {children}
+    </>
+  ) : (
+    <></>
+  );
+}
+
+export function AddButtonForm({
+  handleSubmit = () => {},
+  handleClose = () => {},
+}) {
+  return (
+    <div className="bg-white w-[400px] h-[200px] border p-4 rounded-lg flex flex-col justify-between">
+      <div className="flex flex-col gap-y-4">
+        <div className="flex justify-between">
+          <h3>Track a new goal</h3>
+          <div className="hover:cursor-pointer" onClick={handleClose}>
+            <CloseIcon />
+          </div>
+        </div>
+        <div className="w-full flex flex-col gap-y-1">
+          <div className="font-medium">State what you want to accomplish.</div>
+          <input
+            className="border-2 border-solid rounded-md px-2 py-1 focus:outline-none w-full"
+            placeholder="I want to..."
+          />
+        </div>
+      </div>
+      <div className="" onClick={handleSubmit}>
+        <TextButton variant="solid" width="w-full" text="Create page" />
+      </div>
+    </div>
+  );
+}
+
+function AddGoalButton() {
+  const { value, setValue } = useToggle(false);
+  const router = useRouter();
+
+  const handleClose = () => setValue(false);
+
+  const handleSubmit = () => {
+    // should wait for db to update before moving to page eventually
+    router.replace("/goal");
+  };
+
+  return (
+    <div className="hover:cursor-pointer">
+      <div
+        onClick={() => {
+          setValue(true);
+        }}
+      >
+        <AddIcon />
+      </div>
+      <ClientPortal selector={"#dashboard-root"}>
+        <LightboxMenu showLightbox={value} closeLightbox={handleClose}>
+          <div className="absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
+            <AddButtonForm
+              handleSubmit={handleSubmit}
+              handleClose={handleClose}
+            />
+          </div>
+        </LightboxMenu>
+      </ClientPortal>
     </div>
   );
 }
@@ -29,7 +116,7 @@ export default function GoalsSection({ goals }) {
     <div className="py-4">
       <div className="flex flex-row justify-between px-4">
         <h3>Your Goals</h3>
-        <AddIcon />
+        <AddGoalButton />
       </div>
 
       {goals.length ? (
